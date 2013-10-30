a28181c32a3074699c5a395c96176290fd010126
JoshuaOCS <Joshua@ocsltg.com>
2013-10-30 08:46:18 -0700

    Initial iOS7 Test

---
+++ OCSFixtureSelectionGuide_custom.js
@@ -0,0 +1,49 @@
+/**
+ * Notification that the UI is about to transition to a new screen.
+ * Perform custom prescreen-transition logic here.
+ * @param {String} currentScreenId 
+ * @param {String} targetScreenId 
+ * @returns {boolean} true to continue transtion; false to halt transition
+ */
+phoneui.prePageTransition = function(currentScreenId, targetScreenId) {
+  // add custom pre-transition code here
+  // return false to terminate transition
+  return true;
+}
+
+/**
+ * Notification that the UI has transitioned to a new screen.
+ * 
+ * @param {String} newScreenId 
+ */
+phoneui.postPageTransition = function(newScreenId) {
+  
+}
+
+/**
+ * Notification that the page's HTML/CSS/JS is about to be loaded.
+ * Perform custom logic here, f.e. you can cancel request to the server.
+ * @param {String} targetScreenId 
+ * @returns {boolean} true to continue loading; false to halt loading
+ */
+phoneui.prePageLoad = function(targetScreenId) {
+  // add custom pre-load code here
+  // return false to terminate page loading, this cancels transition to page as well
+  return true;
+}
+
+/**
+ * Notification that device orientation has changed. 
+ * 
+ * @param {String} newOrientation 
+ */
+phoneui.postOrientationChange = function(newOrientation) {
+  
+}
+
+/**
+ * Called when document is loaded.
+ */
+phoneui.documentReadyHandler = function() {
+}
+
